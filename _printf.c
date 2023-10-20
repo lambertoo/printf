89f5a4e178492dd6f67ddd1bd708a2c02491383d
lambertoo <rulindanal@gmail.com>
2023-10-20 06:01:30 -0700

    getting the right answer

--- _printf.c
+++ _printf.c
@@ -7,13 +7,11 @@
  */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int printed = 0;
+	va_list args;
+	int printed = 0;
 
-        va_start(args, format);
-
-<<<<<<< HEAD
 	va_start(args, format);
+
 	while (*format != '\0')
 	{
 		if (*format == '%')
@@ -44,36 +42,4 @@ int _printf(const char *format, ...)
 	}
 va_end(args);
 return (printed);
-=======
-        while (*format != '\0')
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        if (*format == 'r')
-                        {
-                                _putchar('%');
-                                _putchar('r');
-                                printed += 2;
-                        }
-                        else if (*format == '%')
-                        {
-                                _putchar('%');
-                                printed++;
-                        }
-                        else
-                        {
-                                printed = selector(format, args, printed);
-                        }
-                }
-                else
-                {
-                        _putchar(*format);
-                        printed++;
-                }
-                format++;
-        }
-        va_end(args);
-        return (printed);
->>>>>>> e866101c75a516ce2ecd6293ad628586a3fab5fe
 }
