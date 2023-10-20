e866101c75a516ce2ecd6293ad628586a3fab5fe
lambertoo <rulindanal@gmail.com>
2023-10-20 05:09:45 -0700

    modifying _printf.c to hander %r

--- _printf.c
+++ _printf.c
@@ -5,36 +5,41 @@
  * @format: the format specifier
  * Return: the formated string
  */
-
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int printed = 0;
-	
-	va_start(args, format);
+        va_list args;
+        int printed = 0;
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '%')
-			{
-				_putchar('%');
-				printed++;
-			}
-			else
-			{
-				printed = selector(format, args, printed);
-			}
-		}
-		else
-		{
-			_putchar(*format);
-			printed++;
-		}
-		format++;
-	}
-va_end(args);
-return (printed);
+        va_start(args, format);
+
+        while (*format != '\0')
+        {
+                if (*format == '%')
+                {
+                        format++;
+                        if (*format == 'r')
+                        {
+                                _putchar('%');
+                                _putchar('r');
+                                printed += 2;
+                        }
+                        else if (*format == '%')
+                        {
+                                _putchar('%');
+                                printed++;
+                        }
+                        else
+                        {
+                                printed = selector(format, args, printed);
+                        }
+                }
+                else
+                {
+                        _putchar(*format);
+                        printed++;
+                }
+                format++;
+        }
+        va_end(args);
+        return (printed);
 }
